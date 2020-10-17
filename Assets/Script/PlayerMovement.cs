0deaaac21b17d39de95ad65a0a1f47a7a6e58cba
And4Code <Andrea.bois@e-artsup.net>
2020-10-17 23:14:34 +0100

    debut vie / a revoir a 90% pck pas eu le temps
    Rael tu deuh avec tes boulots

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector3 mousePosition;
-    private Rigidbody2D rb;
-    private Vector2 direction;
-    public float moveSpeed = 100f;
+
+    
+    public float speed = 3;
+    private Transform playerTransform;
+    private int accelerationTime = 3;
+    public CameraShake cameraShake;
+    bool IsShaking;
+
+   
+
+
+
+   
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
+        playerTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        Vector3 movementX = Vector3.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(movementX);
+
+        Vector3 movementY = Vector3.up * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        transform.Translate(movementY);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction = (mousePosition - transform.position).normalized;
         rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
