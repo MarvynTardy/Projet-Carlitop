c5c65fe04bc70064f062991e9a8fa18a79ce725f
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2020-10-20 18:56:47 +0200

    push HealthComponent

---
+++ Assets/Script/HealthComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    void Update()
+    {
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage; 
+    }
+
+    void Heal(int heal)
+    {
+        currentHealth = +heal;
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
