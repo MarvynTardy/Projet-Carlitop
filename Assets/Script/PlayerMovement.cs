41ef01e5ad8e86b6b4f1041e30b4bb723d456abe
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2020-10-21 11:50:20 +0200

    modifplayer

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public string axis;
+
     public float speed = 3;
     private Transform playerTransform;
     private int accelerationTime = 3;
     public CameraShake cameraShake;
     bool IsShaking;
+    public LayerMask mask;
 
-   
+
+
 
 
 
     void Start()
     {
+
         playerTransform = GetComponent<Transform>();
     }
 
@@ -26,24 +29,38 @@ public class PlayerMovement : MonoBehaviour
     {
 
 
-        Vector3 Movement = Vector3.right * Input.GetAxisRaw(axis) * speed * Time.deltaTime;
-        transform.Translate(Movement);
+        Vector3 movementX = Vector3.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(movementX);
 
-        
-       
-
-
+        Vector3 movementY = Vector3.up * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        transform.Translate(movementY);
 
 
+        foreach (Collider2D collision in Physics2D.OverlapCircleAll(transform.position, 0.1f, mask))
+        {
+            if (collision.CompareTag("player"))
+            {
+                GetComponent<HealthComponent>().TakeDamage(30);
+            }
+            
+          
 
 
 
 
 
 
+
+
+        }
     }
-
-    
-
-    
 }
+
+
+
+
+       /* mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);*/
+    
+
